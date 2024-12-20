e3cd208798ee96037fa3444b0fb5bfe1ecfb374e
Pahul Gogna <135852041+PahulGogna@users.noreply.github.com>
2024-12-21 01:10:33 +0530

    build: update Dockerfile for production build and remove unused dependencies

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,8 @@
 import express from 'express'
 import { readUrlJsonData } from './utils/readCSV';
-import { UrlInputSchema, urlInputSchema, urlSchema } from '@pahul100/short-link-common'
+import { UrlInputSchema, urlInputSchema } from '@pahul100/short-link-common'
 import { shortHash } from './utils/hashing';
 import { Url } from './utils/database';
-import { sign, verify } from 'jsonwebtoken';
 import cors from 'cors';
 
 const app = express();
@@ -26,6 +25,8 @@ app.post('/shrink',async (req, res) => {
     let data = await readUrlJsonData()
 
     let body = urlInputSchema.safeParse(req.body)
+
+    const fullUrl = `${req.protocol}://${req.get("host")}/`
 
     if(!body.success){
         res.status(400).json({"detail": "Invalid Input"})
@@ -50,7 +51,7 @@ app.post('/shrink',async (req, res) => {
     })
 
     if(exists.length > 0){
-        const key = exists[0].key
+        const key = fullUrl + exists[0].key
 
         res.json({"detail":key})
         return 
@@ -66,72 +67,39 @@ app.post('/shrink',async (req, res) => {
         return
     }
 
-
-    let createdUrl = await Url.create({
-                        key: hash,
-                        url: url.url,
-                        verified: true
-                    })
-
-    const key = createdUrl.key
-
-    res.json({"detail":key})
-})
-
-app.get('/token/*', (req, res) => {
-    const url = req.originalUrl.replace(/[\s/]+$/, '').split('/')
-
-    const id = url[url.length - 1]
-
-    if(!id){
-        res.status(404).json({"detail": "Not Found"})
-        return
+    try{
+        let createdUrl = await Url.create({
+                            key: hash,
+                            url: url.url,
+                            verified: true
+                        })
+    
+        const key = fullUrl + createdUrl.key
+    
+        res.json({"detail":key})
     }
-
-    let expire = Date.now() + 1000 * 60 // 60 seconds validity
-    let countDown = Date.now() + 1000 * 15 //15 seconds countdown
-
-
-    const token = sign({ id: id, countDown: Math.floor(countDown),exp: Math.floor(expire/1000)}  , process.env.JWT_SECRET as string)
-
-    res.send({"detail":token})
+    catch{
+        res.status(500).json({"detail":"internal server error"})
+    }
 })
 
 app.get('/*', async (req, res) => {
 
-    try{
-        const header = req.header("authorization") || "";
-        const token = header.split(' ')[1];
+    const id = req.originalUrl.slice(1)
 
-        const decodedToken = verify(token, process.env.JWT_SECRET as string);
-        if (typeof decodedToken === 'string' || !decodedToken || !('id' in decodedToken) || !('countDown' in decodedToken)) {
-            res.status(403).json({"detail": "Unauthorized"});
-            return;
-        }3
-        const tokenData = decodedToken as { id: string, countDown: number };
-
-        const date = Date.now()
-
-        if(date < tokenData.countDown){
-            res.status(403).json({"detail": "Unauthorized"})
-            return
-        }
-    }
-    catch(e){
-        res.status(403)
-        res.json({"detail": "Unauthorized"})
+    if(!id){
+        res.sendStatus(404)
         return
     }
 
-    const id = req.originalUrl.slice(1)
     let data = await Url.find({
         key: id
     })
     if(data.length > 0){
-        res.send(data[0].url)
+        res.redirect(data[0].url)
     }
     else{
-        res.status(404).json({"detail": "not found"})
+        res.sendStatus(404)
     }
 })
 
