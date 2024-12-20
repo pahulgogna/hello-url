b51ba29b63fca713207f99cc91cea0337c7612a8
Pahul Gogna <135852041+PahulGogna@users.noreply.github.com>
2024-12-21 01:10:16 +0530

    refactor: remove routing and directly render ShrinkUrl component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Appbar from './components/Appbar'
 import ShrinkUrl from './pages/ShrinkUrl'
-import OpenUrl from './pages/OpenUrl'
 
 function App() {
 
   return (
     <div className='h-full'>
       <Appbar/>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<ShrinkUrl/>} path='/'/>
-          <Route element={<OpenUrl/>} path='/*'/>
-        </Routes>
-      </BrowserRouter>
+      <ShrinkUrl/>
     </div>
   )
 }
